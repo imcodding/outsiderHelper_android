2d9609c033569561c92b2ea58f4856d4d2d8c141
PC-042\PC <suy0226>
2020-11-28 01:24:43 +0900

    기타: 공통 액티비티 변경

--- OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/BaseActivity.java
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/BaseActivity.java
@@ -2,19 +2,33 @@ package com.mia.outsiderhelper;
 
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("Registered")
 public class BaseActivity extends AppCompatActivity {
     public ProgressDialog mProgressDialog;
 
-
     public void showCustomToast(final String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    public void showDialog(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void showProgressDialog() {
