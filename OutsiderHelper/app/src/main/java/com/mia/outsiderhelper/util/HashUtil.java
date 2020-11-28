ded0eddf7c7a407edf0991c5429df00f3bc82539
PC-064\PC <suy0226@naver.com>
2020-11-29 00:55:56 +0900

    기능 변경: AES256 -> SHA-256

--- OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/util/HashUtil.java
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/util/HashUtil.java
@@ -1,100 +1,21 @@
 package com.mia.outsiderhelper.util;
 
-import android.os.Build;
-
-import java.nio.ByteBuffer;
-import java.security.AlgorithmParameters;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Base64;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
 
 public class HashUtil {
-    private static final String key = "secret key";
 
-    /**
-     *  AES 256 으로 암호화 한다.
-     */
-    public static String encryptAES256(String msg) throws Exception {
-
-        SecureRandom random = new SecureRandom();
-        byte bytes[] = new byte[20];
-        random.nextBytes(bytes);
-        byte[] saltBytes = bytes;
-
-        // Password-Based Key Derivation function 2
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-
-        // 70000번 해시하여 256 bit 길이의 키를 만든다.
-        PBEKeySpec spec = new PBEKeySpec(key.toCharArray(), saltBytes, 70000, 256);
-        SecretKey secretKey = factory.generateSecret(spec);
-        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-        // 알고리즘/모드/패딩
-        // CBC : Cipher Block Chaining Mode
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-        cipher.init(Cipher.ENCRYPT_MODE, secret);
-
-        AlgorithmParameters params = cipher.getParameters();
-
-        // Initial Vector(1단계 암호화 블록용)
-        byte[] ivBytes = params.getParameterSpec(IvParameterSpec.class).getIV();
-        byte[] encryptedTextBytes = cipher.doFinal(msg.getBytes("UTF-8"));
-        byte[] buffer = new byte[saltBytes.length + ivBytes.length + encryptedTextBytes.length];
-
-        System.arraycopy(saltBytes, 0, buffer, 0, saltBytes.length);
-        System.arraycopy(ivBytes, 0, buffer, saltBytes.length, ivBytes.length);
-        System.arraycopy(encryptedTextBytes, 0, buffer, saltBytes.length + ivBytes.length, encryptedTextBytes.length);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            return Base64.getEncoder().encodeToString(buffer);
-        } else {
-            return null;
-        }
-
+    public static String sha256(String msg)  throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(msg.getBytes());
+        return byteToHexString(md.digest());
     }
 
-    /**
-     *  AES 256 을 복호화 한다.
-     */
-    public static String decryptAES256(String msg, String key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException,
-            InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        ByteBuffer buffer = null;
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            buffer = ByteBuffer.wrap(Base64.getDecoder().decode(msg));
+    public static String byteToHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b : data) {
+            sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
         }
-
-        byte[] saltBytes = new byte[20];
-        buffer.get(saltBytes, 0, saltBytes.length);
-        byte[] ivBytes = new byte[cipher.getBlockSize()];
-        buffer.get(ivBytes, 0, ivBytes.length);
-        byte[] encryoptedTextBytes = new byte[buffer.capacity() - saltBytes.length - ivBytes.length];
-        buffer.get(encryoptedTextBytes);
-
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        PBEKeySpec spec = new PBEKeySpec(key.toCharArray(), saltBytes, 70000, 256);
-
-        SecretKey secretKey = factory.generateSecret(spec);
-        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(ivBytes));
-
-        byte[] decryptedTextBytes = cipher.doFinal(encryoptedTextBytes);
-        return new String(decryptedTextBytes);
+        return sb.toString();
     }
 }
