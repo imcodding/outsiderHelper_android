165c4dacc72acbdfc867d1fbd86fe302d7fda2c9
PC-064\PC <suy0226@naver.com>
2020-11-29 00:59:46 +0900

    기능추가: 홈 화면

---
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/home/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.mia.outsiderhelper.main.fragment.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.mia.outsiderhelper.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
