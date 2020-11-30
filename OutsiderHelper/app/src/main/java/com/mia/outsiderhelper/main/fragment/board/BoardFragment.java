c04d5038278bd8de847ae8f7c4aac1c76804af72
PC-031\PC <suy0226@naver.com>
2020-11-30 19:27:57 +0900

    추가: 게시판 프래그먼트

---
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/board/BoardFragment.java
@@ -0,0 +1,24 @@
+package com.mia.outsiderhelper.main.fragment.board;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.mia.outsiderhelper.BaseFragment;
+import com.mia.outsiderhelper.R;
+
+public class BoardFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_board, container, false);
+
+        return view;
+    }
+
+}
