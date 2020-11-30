a50933886aba1adb472f633362ca64e07d6bdd55
PC-031\PC <suy0226@naver.com>
2020-11-30 19:27:40 +0900

    추가: 상권 프래그먼트

---
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/store/StoreFragment.java
@@ -0,0 +1,50 @@
+package com.mia.outsiderhelper.main.fragment.store;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.mia.outsiderhelper.BaseFragment;
+import com.mia.outsiderhelper.R;
+
+public class StoreFragment extends BaseFragment implements View.OnClickListener {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_store_list, container, false);
+
+        LinearLayout storeEating = view.findViewById(R.id.store_linear_eating);
+        LinearLayout storeCafe = view.findViewById(R.id.store_linear_cafe);
+        LinearLayout storeConvenience = view.findViewById(R.id.store_linear_convenience);
+        LinearLayout storeEntertain = view.findViewById(R.id.store_linear_entertain);
+
+        storeEating.setOnClickListener(this);
+        storeCafe.setOnClickListener(this);
+        storeConvenience.setOnClickListener(this);
+        storeEntertain.setOnClickListener(this);
+
+        return view;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.store_linear_eating:
+                break;
+            case R.id.store_linear_cafe:
+                break;
+            case R.id.store_linear_convenience:
+                break;
+            case R.id.store_linear_entertain:
+                break;
+        }
+    }
+}
