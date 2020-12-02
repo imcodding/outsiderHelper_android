ddfceda2ebc49eeb5ab358179709013b8a6b77b2
DESKTOP-TV3GRHM\yd043 <suy0226@naver.com>
2020-12-02 17:38:42 +0900

    문서변경: 변수 정리

--- OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/home/HomeFragment.java
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/home/HomeFragment.java
@@ -1,20 +1,15 @@
 package com.mia.outsiderhelper.main.fragment.home;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 
-import com.bumptech.glide.Glide;
 import com.mia.outsiderhelper.BaseFragment;
 import com.mia.outsiderhelper.R;
 import com.mia.outsiderhelper.main.fragment.home.adapter.ImageAdapter;
@@ -26,13 +21,15 @@ import java.util.TimerTask;
 
 public class HomeFragment extends BaseFragment implements HomeFragmentView {
 
-    private final String TAG = "HomeFragment";
+    private final static String TAG = "HomeFragment";
 
     private HomeService mHomeService;
     private ViewPager mHomeFoodViewPager;
-    private ImageAdapter mImageAdapter;
     private Timer mTimer;
+
     private int currentPos = 0;
+    private final int PAGING_DELAY = 1000;
+    private final int PAGING_PERIOD = 2000;
 
     @Nullable
     @Override
@@ -68,8 +65,8 @@ public class HomeFragment extends BaseFragment implements HomeFragmentView {
     public void getFoodImageSuccess(ArrayList<String> images) {
         hideProgressDialog();
 
-        mImageAdapter = new ImageAdapter(getContext(), images);
-        mHomeFoodViewPager.setAdapter(mImageAdapter);
+        ImageAdapter imageAdapter = new ImageAdapter(getContext(), images);
+        mHomeFoodViewPager.setAdapter(imageAdapter);
 
         mHomeFoodViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -100,7 +97,7 @@ public class HomeFragment extends BaseFragment implements HomeFragmentView {
                     }
                 });
             }
-        },1000,2000);
+        }, PAGING_DELAY, PAGING_PERIOD);
 
     }
 
