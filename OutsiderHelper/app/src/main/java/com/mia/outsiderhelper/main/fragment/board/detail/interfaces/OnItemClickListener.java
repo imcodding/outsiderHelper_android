53dc8556dfbf6dfd2d0124ce29167b5ef031ac48
DESKTOP-TV3GRHM\yd043 <suy0226@naver.com>
2020-12-04 12:37:13 +0900

    리팩토링: 위치 변경

--- OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/board/detail/interfaces/OnItemClickListener.java
+++ OutsiderHelper/app/src/main/java/com/mia/outsiderhelper/main/fragment/board/detail/interfaces/OnItemClickListener.java
@@ -1,4 +1,4 @@
-package com.mia.outsiderhelper.main.fragment.board.detail;
+package com.mia.outsiderhelper.main.fragment.board.detail.interfaces;
 
 public interface OnItemClickListener {
     void onItemClick(int pos);
